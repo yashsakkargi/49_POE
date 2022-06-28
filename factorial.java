83d7968bd3f707edcc9ad992acbae8fcb61195ec
yashsakkargi <101868183+yashsakkargi@users.noreply.github.com>
2022-06-28 15:49:06 +0530

    Update factorial.java

--- factorial.java
+++ factorial.java
@@ -1,25 +1,10 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
- <head>
-  <title> New Document </title>
-  <script type="text/javascript">
- function fact(x) {
-   if(x==0) {
-      return 1;
-   }
-   return x * fact(x-1);
-}
-
-function run(number) {
-    alert(fact(parseInt(number, 10)));
-}
-  </script>
- </head>
-
- <body>
- <form name="f1">
-  Enter the Number  :<input type="text" length="8" name="txt1"><br>
-  <input type="button" value="Find factiorial" onclick="run(txt1.value)">
-  </form>
- </body>
-</html>
+class factorial{  
+ public static void main(String args[]){  
+  int i,fact=1;  
+  int number=5;//It is the number to calculate factorial    
+  for(i=1;i<=number;i++){    
+      fact=fact*i;    
+  }    
+  System.out.println("Factorial of "+number+" is: "+fact);    
+ }  
+}  
